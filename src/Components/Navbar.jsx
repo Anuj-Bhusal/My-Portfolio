d0167ecf875fbdd3ef0d443b05f9a62feac165d0
Anuj Bhusal <anujbhusal4@gmail.com>
2024-10-24 20:05:03 +0545

    Hello

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   }
 
   const closeMenu = () => {
-    menuRef.current.style.right = "-350px";
+    menuRef.current.style.right = "-300px";
   }
 
   const handleLinkClick = () => {
@@ -22,38 +22,40 @@ const Navbar = () => {
   }
 
   return (
-    <div className='navbar'>
-      <img src={LOGO1} alt="logo" />
-      <img src={menu_open} onClick={openMenu} alt="Open Menu" className='nav-mob-open' />
-      
-      <ul ref={menuRef} className='nav-menu'>
-        <img src={menu_close} onClick={closeMenu} alt="Close Menu" className="nav-mob-close" />
-        <li>
-          <NavLink to="/" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/skills" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Skills</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/blog" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Blog</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Contact</NavLink>
-        </li>
-      </ul>
-      <button className='nav-connect' 
-        onClick={() => {
-          handleLinkClick(); 
-          navigate('/contact', { replace: true });
-        }}>
-        Get in touch
-      </button>
+    <div className='container'> {/* Added container class */}
+      <div className='navbar'>
+        <img src={LOGO1} alt="logo" />
+        <img src={menu_open} onClick={openMenu} alt="Open Menu" className='nav-mob-open' />
+        
+        <ul ref={menuRef} className='nav-menu'>
+          <img src={menu_close} onClick={closeMenu} alt="Close Menu" className="nav-mob-close" />
+          <li>
+            <NavLink to="/" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/skills" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Skills</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Blog</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={handleLinkClick} className={({ isActive }) => (isActive ? "active" : "")}>Contact</NavLink>
+          </li>
+        </ul>
+        <button className='nav-connect' 
+          onClick={() => {
+            handleLinkClick(); 
+            navigate('/contact', { replace: true });
+          }}>
+          Get in touch
+        </button>
+      </div>
     </div>
   );
 };
